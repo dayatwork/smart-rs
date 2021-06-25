8aa0d6ad07e3ddf246a12d1eafde2003262635c4
dayatdev <iamdayatdev@gmail.com>
2021-06-25 16:49:39 +0700

    fix typo limit pagination

--- src/pages/web-staff/event-node/sub-page/booking/CreateBooking.jsx
+++ src/pages/web-staff/event-node/sub-page/booking/CreateBooking.jsx
@@ -453,7 +453,7 @@ export const CreateBooking = () => {
                           value={limit}
                           onChange={e => setLimit(e.target.value)}
                         >
-                          <option value={6}>5</option>
+                          <option value={6}>6</option>
                           <option value={10}>10</option>
                           <option value={20}>20</option>
                           <option value={50}>50</option>
