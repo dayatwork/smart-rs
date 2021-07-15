21f4136767884f2a3bdae830bc9ea850b31fb83b
Muhammad Hidayatullah <dayatwork095@gmail.com>
2021-07-15 14:36:59 +0700

    add advertisement in homepage

---
+++ src/components/web-patient/home/AdvertisementCard.jsx
@@ -0,0 +1,97 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  LinkBox,
+  LinkOverlay,
+  Text,
+} from '@chakra-ui/react';
+import * as React from 'react';
+import { BsClockFill } from 'react-icons/bs';
+
+export const AdvertisementCard = ({
+  title,
+  href,
+  description,
+  media,
+  author,
+  category,
+}) => {
+  return (
+    <LinkBox
+      as="article"
+      bg="white"
+      shadow={{
+        sm: 'base',
+      }}
+      rounded={{
+        sm: 'md',
+      }}
+      overflow="hidden"
+      transition="all 0.2s"
+      _hover={{
+        shadow: {
+          sm: 'lg',
+        },
+      }}
+    >
+      <Flex direction="column">
+        <Image
+          height="60"
+          objectFit="cover"
+          alt={title}
+          src={media}
+          fallbackSrc="https://placekitten.com/640/360"
+        />
+        <Flex
+          direction="column"
+          px={{
+            sm: '6',
+          }}
+          py="5"
+        >
+          <Text
+            casing="uppercase"
+            letterSpacing="wider"
+            fontSize="xs"
+            fontWeight="semibold"
+            mb="2"
+            color="gray.500"
+          >
+            {category}
+          </Text>
+          <Heading as="h3" size="sm" mb="2" lineHeight="base">
+            <LinkOverlay href={href}>{title}</LinkOverlay>
+          </Heading>
+          <Text noOfLines={2} mb="8" color="gray.600">
+            {description}
+          </Text>
+          <Flex
+            align="baseline"
+            justify="space-between"
+            fontSize="sm"
+            color="gray.600"
+          >
+            <Text>
+              By{' '}
+              <Box as="a" textDecor="underline" href={author.href}>
+                {author.name}
+              </Box>
+            </Text>
+            <Link href="#">
+              <Box
+                as={BsClockFill}
+                display="inline-block"
+                me="2"
+                opacity={0.4}
+              />
+              3 min read
+            </Link>
+          </Flex>
+        </Flex>
+      </Flex>
+    </LinkBox>
+  );
+};
--- src/pages/web-patient/HomePage.jsx
+++ src/pages/web-patient/HomePage.jsx
@@ -1,24 +1,97 @@
-import { Flex } from '@chakra-ui/react';
 import * as React from 'react';
+// import { Link } from 'react-router-dom';
+import {
+  Box,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Link as ChakraLink,
+  Skeleton,
+} from '@chakra-ui/react';
+import { useCookies } from 'react-cookie';
+import { useQuery } from 'react-query';
 
-import { HomeHero } from '../../components/web-patient/home';
-import { WebPatientNav, Wrapper } from '../../components/web-patient/shared';
-import { AuthContext } from '../../contexts/authContext';
+import { getAdvertisements } from 'api/institution-services/advertisement';
+import { HomeHero, AdvertisementCard } from 'components/web-patient/home';
+import { WebPatientNav, Wrapper } from 'components/web-patient/shared';
+import { AuthContext } from 'contexts/authContext';
+import { BsArrowRight } from 'react-icons/bs';
 
 const HomePage = () => {
   const { user } = React.useContext(AuthContext);
+  const [cookies] = useCookies(['token']);
+
+  const { data: dataAdvertisement, isLoading } = useQuery(
+    ['advertisement'],
+    () => getAdvertisements(cookies),
+    { staleTime: Infinity }
+  );
 
   return (
     <Flex
       direction="column"
       bg="secondary.lighter"
-      height="100vh"
+      minH="100vh"
       maxW="100vw"
-      overflow="hidden"
+      overflow="auto"
     >
       <WebPatientNav active="home" />
       <Wrapper>
         <HomeHero user={user} />
+        <Box mt="12">
+          <Heading size="lg" mb="6" fontWeight="bold">
+            News & Promotion
+          </Heading>
+          {isLoading ? (
+            <SimpleGrid
+              columns={{
+                base: 1,
+                md: 3,
+              }}
+              spacing="12"
+              mb="6"
+            >
+              <Skeleton height="400px" rounded="md" />
+              <Skeleton height="400px" rounded="md" />
+              <Skeleton height="400px" rounded="md" />
+            </SimpleGrid>
+          ) : (
+            <>
+              <SimpleGrid
+                columns={{
+                  base: 1,
+                  md: 3,
+                }}
+                spacing="12"
+                mb="6"
+              >
+                {dataAdvertisement?.data?.slice(0, 3)?.map(advertisement => (
+                  <AdvertisementCard
+                    key={advertisement.id}
+                    category={advertisement.category}
+                    media={`${process.env.REACT_APP_UPLOADED_FILE_URL}/${advertisement.image}`}
+                    title={advertisement.title}
+                    description={advertisement.content}
+                    href="#"
+                    author={{
+                      name: 'SMART-RS',
+                      href: '#',
+                    }}
+                  />
+                ))}
+              </SimpleGrid>
+              <ChakraLink
+                // as={Link}
+                fontSize="lg"
+                fontWeight="bold"
+                color="primary.600"
+              >
+                <span>View all news & promotion</span>
+                <Box as={BsArrowRight} display="inline-block" ms="2" />
+              </ChakraLink>
+            </>
+          )}
+        </Box>
       </Wrapper>
     </Flex>
   );
